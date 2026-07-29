939d2a035f9848761e0df6a35324b4dfb7577049
etherealorkismet <3znorb@gmail.com>
2026-07-29 17:51:05 +0800

    testing cs

---
+++ Assets/Game/Scripts/tester.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class tester : MonoBehaviour
+{
+    public List<GameObject>thisList;
+    
+    void Start()
+    {
+        
+        thisList.Add
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
